5bffc4f099a070a0e35f3cfaf654b48e58caca66
augustomorales92 <augustomorales92@gmail.com>
2024-03-10 11:47:15 +0100

    update params check

--- app/ui/products/pagination.tsx
+++ app/ui/products/pagination.tsx
@@ -7,7 +7,7 @@ import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get('page')) || 1;
+  const currentPage = Number(searchParams?.get('page')) || 1;
   const pathname = usePathname();
 
   const allPages = generatePagination(currentPage, totalPages);
